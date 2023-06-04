12968306196e6fe92880b95a6f0aba946e437570
Stephen_Sun <HaixinSun0324@gmail.com>
2023-06-04 22:51:38 +0800

    convert comment to English

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # simple-graph-judgment
-Judge the simple graph by the sequence of degrees
+Input a sequence, judge whether the sequence is the degree sequence of a simple graph.
+
+If it is, then draw a corresponding simple graph.
+
+Example:
+
+input
+
+[5 4 3 2 1 1 1 1]
+
+output
+
+<img src="D:\0_myfile\Graduate_grad1\课程\图论\课程设计\程序运行图4_简单图.png" style="zoom:50%;" /> 
+
+
+
--- main.py
+++ main.py
@@ -1,73 +1,74 @@
 '''
-通过软件界面输入任意一个有限非负整数序列，
-如果是图序列，界面上显示一个对应的简单图，
-否则，界面上显示“否”。
+Input any finite non-negative integer sequence.
+If it is a graph sequence, a corresponding simple graph will be displayed on the interface
 '''
 
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 
-seq_input = [int(n) for n in input("输入一个序列：").split()]  # 输入一个序列
-seq_input.sort(reverse=True)  # 将输入的序列降序排列
-print("序列按降序排列：", seq_input)
+seq_input = [int(n) for n in input("Input a sequence:").split()]  # Input a sequence
+seq_input.sort(reverse=True)  # Sort the input sequence in descending order
+print("The sequence is in descending order:", seq_input)
 
-# 若序列的和为奇数，则不是图的度序列
+# If the sum of the sequence is odd, it is not a sequence of degrees of the graph
 sum_seq_input = np.sum(seq_input)
 if sum_seq_input % 2 == 1:
-    print("该序列不是图序列！")
+    print("This sequence is not a graph sequence!")
 else:
-    seq_input = np.array(seq_input)  # 将输入转化为np数组
-    seq_len = seq_input.size  # 获取序列长度
+    seq_input = np.array(seq_input)  # Converts the input to an np array
+    seq_len = seq_input.size  # Fetch sequence length
     n = seq_len
-    matrix_adj = np.zeros((n, n))  # 生成一个nxn的邻接矩阵
-    matrix_iter = np.array([seq_input])  # 初始化序列矩阵，保存每一次循环过后的序列结果
+    matrix_adj = np.zeros((n, n))  # Generate an adjacency matrix of nxn
+    matrix_iter = np.array([seq_input])  # Initialize the sequence matrix to store the sequence results after each loop
 
     ite_num = 0
-    # 循环求解删除第一个点以及对应的边后的子图的序列
+    # Loop to solve the sequence of subgraphs after deleting the first point and corresponding edge
     while ((seq_input < 0).any() == False) & ((seq_input == 0).all() == False):
 
-        # 当最大度点的度数大于n-1时，不存在简单图
+        # When the degree of the maximum point is greater than n-1, there is no simple graph
         if (seq_input[0]+1) > n:
-            print("该序列不是图序列！")
+            print("This sequence is not a graph sequence!")
             break
 
-        # 循环次数计数
+        # Cycle count
         ite_num = ite_num + 1
-        print("第", ite_num, "轮递归")
+        print("Number of cycles:", ite_num)
 
-        # 执行简单图度序列判定算法
+        # Execute simple graph sequence decision algorithm
         for i in range(0, seq_input[0]+1):
             seq_input[i] = seq_input[i]-1
-        seq_input = np.delete(seq_input, 0)  # 删去剩余序列的第一个点
+        seq_input = np.delete(seq_input, 0)  # Delete the first point of the remaining sequence
         n = n-1
-        # 由于np.sort函数只能升序排列，因此对其相反数进行升序排列后再取相反数
+        # Since the np.sort function can only be sorted in ascending order
+        # its inverse number is sorted in ascending order and then the inverse number is taken
         seq_input = np.sort(-seq_input)
         seq_input = (-seq_input)
         print(seq_input)
 
-        # 更新序列矩阵
+        # Update sequence matrix
         d_inst = np.pad(seq_input, (ite_num, 0), 'constant', constant_values=(0, 0))
         matrix_iter = np.insert(matrix_iter, ite_num, values=d_inst, axis=0)
 
-    # 循环结束，判断是否是全0还是有负数
+    # The loop ends, and we decide whether it's all zeros or negative numbers
     if (seq_input < 0).any():
-        print("该序列不是图序列！")
+        print("This sequence is not a graph sequence!")
     elif (seq_input == 0).all():
 
-        # 当可以是图序列的时候，利用迭代矩阵更新邻接矩阵
-        for i in range(ite_num-1, -1, -1):  # 行遍历
-            for k in range(i+1, seq_len, 1):  # 列遍历
+        # When a graph sequence is available, the adjacency matrix is updated using the iterative matrix
+        for i in range(ite_num-1, -1, -1):  # Row traversal
+            for k in range(i+1, seq_len, 1):  # Column traversal
                 matrix_adj[i, k] = matrix_iter[i, k] - matrix_iter[i+1, k]
                 matrix_adj[k, i] = matrix_iter[i, k] - matrix_iter[i+1, k]
-        print("该序列对应的图的邻接矩阵：")
+        print("The adjacency matrix of the graph corresponding to this sequence:")
         print(matrix_adj)
 
-        # 使用networkx进行绘图
+        # Plot using networkx
         G = nx.Graph()
         Matrix = matrix_adj
 
-        # 当邻接矩阵中对应的点之间的值为1时，在两点之间添加一条边
+        # When the value between the corresponding points in the adjacency matrix is 1
+        # An edge is added between the two points
         for i in range(len(Matrix)):
             for j in range(len(Matrix)):
                 if Matrix[i, j] == 1:
